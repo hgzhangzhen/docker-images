0ab9153425610c2e65289429c5222bd0c10984cd
hgzhangzhen <hgzhangzhen@126.com>
2023-04-29 03:52:10 +0000

    docker_mysql

---
+++ mysql_docker/mysql/initdb/init.sql
@@ -0,0 +1,5 @@
+create database apptest;
+create table apptest.user(id int,name varchar(20));
+insert into apptest.user values(1,'a')
+insert into apptest.user values(2,'b')
+insert into apptest.user values(3,'c')
